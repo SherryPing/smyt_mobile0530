c31d03ec8be7619d3e7d9a666a253ec0eef48c19
SherryPing <2998554420@qq.com>
2018-05-31 12:41:47 +0800

    导航栏样式调整

--- fofeasy_mobile/js/public.js
+++ fofeasy_mobile/js/public.js
@@ -34,6 +34,9 @@ $(function(){
 	
 })
 window.onload=function(){
-	var cover_height=$("body").innerHeight()-64;		//获取当前屏幕的高度
-	$(".cover").height(cover_height+"px");		//设置遮罩层的高度   		
+	var cover_height=$("body").innerHeight()-52;		//获取当前屏幕的高度
+	if(cover_height>750){
+		$(".cover").height(cover_height+"px");		//设置遮罩层的高度   		
+	}
+//	$(".cover").height(cover_height+"px");		//设置遮罩层的高度   		
 }
